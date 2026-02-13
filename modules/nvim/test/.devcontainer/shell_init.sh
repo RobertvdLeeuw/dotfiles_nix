72ecfd4b131025bd25b53a24610a76dc8be44683
Robert <robert.van.der.leeuw@gmail.com>
2026-02-14 00:40:49 +0100

    .

--- modules/nvim/test/.devcontainer/shell_init.sh
+++ modules/nvim/test/.devcontainer/shell_init.sh
@@ -1,71 +1,39 @@
-#!/bin/bash
+apt-get update && apt-get install -y zsh curl wget fastfetch
 
-# Switch to zsh for configuration
-exec zsh -c '
-    # Zsh completion setup
-    autoload -U compinit
-    zmodload zsh/complist
-    compinit
-    _comp_options+=(globdots)
+eval "$(zoxide init --cmd cd zsh)"
+eval "$(fzf --zsh)"
 
-    zstyle ":completion:*" matcher-list "m:{a-z}={a-zA-Z}"
-    zstyle ":completion:*" list-colors "${(s.:.)LS_COLORS}"
-    zstyle ":completion:*" menu no
-
-    zstyle ":fzf-tab:complete:cd:*" fzf-preview "ls --color \$realpath"
-
-    # Key bindings
-    bindkey "^I" complete-word        # tab
-    bindkey "^[[Z" forward-word       # shift + tab
-    bindkey "^[^I" autosuggest-accept # shift + tab
-
-    clear-terminal() {
-        tput reset
-        zle redisplay
-    }
-    zle -N clear-terminal
-    bindkey "^[l" clear-terminal
-    bindkey -M viins "^[l" clear-terminal
-
-    # History search
-    bindkey "^[k" history-search-backward
-    bindkey "^[j" history-search-forward
-    bindkey -M viins "^[k" history-search-backward
-    bindkey -M viins "^[j" history-search-forward
-
-    # Make configurations persistent
-    cat >> ~/.zshrc << EOF
-# Initialize tools (installed via features)
-eval "\$(zoxide init --cmd cd zsh)"
-eval "\$(fzf --zsh)"
-eval "\$(starship init zsh)"
-
-# Completion setup
 autoload -U compinit
 zmodload zsh/complist
 compinit
-_comp_options+=(globdots)
+_comp_options+=(globdots) # Include hidden files.
 
-zstyle \":completion:*\" matcher-list \"m:{a-z}={a-zA-Z}\"
-zstyle \":completion:*\" list-colors \"\${(s.:.)LS_COLORS}\"
-zstyle \":completion:*\" menu no
-zstyle \":fzf-tab:complete:cd:*\" fzf-preview \"ls --color \\\$realpath\"
+zstyle ':completion:*' matcher-list 'm:{a-z}={a-zA-Z}'
+zstyle ':completion:*' list-colors '{(s.:.)LS_COLORS}'
+zstyle ':completion:*' menu no
 
-# Key bindings
-bindkey \"^I\" complete-word
-bindkey \"^[[Z\" forward-word
-bindkey \"^[^I\" autosuggest-accept
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
 
-clear-terminal() { tput reset; zle redisplay; }
+bindkey '^I' complete-word        # tab
+bindkey '^[[Z' forward-word       # shift + tab
+bindkey '^[^I' autosuggest-accept # shift + tab
+
+clear-terminal() {
+  tput reset
+  zle redisplay
+}
 zle -N clear-terminal
-bindkey \"^[l\" clear-terminal
-bindkey -M viins \"^[l\" clear-terminal
+bindkey '^[l' clear-terminal
+bindkey -M viins '^[l' clear-terminal
 
-bindkey \"^[k\" history-search-backward
-bindkey \"^[j\" history-search-forward
-bindkey -M viins \"^[k\" history-search-backward
-bindkey -M viins \"^[j\" history-search-forward
-EOF
+# bindkey '^[[1;5A' history-search-backward
+# bindkey '^[[1;5B' history-search-forward
 
-    echo "Zsh configuration complete!"
-'
+bindkey '^[k' history-search-backward
+bindkey '^[j' history-search-forward
+bindkey -M viins '^[k' history-search-backward
+bindkey -M viins '^[j' history-search-forward
+
+sudo apt install fonts-firacode
+curl -sS https://starship.rs/install.sh | sh
+eval "$(starship init zsh)"
