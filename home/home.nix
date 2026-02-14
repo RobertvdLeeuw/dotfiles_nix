1b7bd7b6727abca2a44f18eabce4b0a07f62405e
Robert <robert.van.der.leeuw@gmail.com>
2026-02-14 17:58:27 +0100

    .

--- home/home.nix
+++ home/home.nix
@@ -53,6 +53,7 @@
     oterm
     code-cursor
     vscode
+    devcontainer
     # atlauncher
     prismlauncher
     lutris
--- modules/nvim/test/.devcontainer/shell_init.sh
+++
@@ -1,2 +0,0 @@
-apt-get update && apt-get install -y zsh curl wget fastfetch git fzf zoxide fonts-firacode starship
-
