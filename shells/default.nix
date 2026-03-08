b81cdda62cf63bdc21544ec3ba6f1db227f9cf5b
Robert <robert.van.der.leeuw@gmail.com>
2026-03-08 02:14:12 +0100

    ZSH update aliases per machine.

--- shells/default.nix
+++ shells/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   imports = [ ./zsh.nix ];
   home = {
@@ -6,27 +11,64 @@
       any-nix-shell # for nix-shell in zsh. I don't like bash. No. Stop it.
     ];
 
-    shellAliases = {
-      update = "cd /etc/nixos; sudo nix flake update; sudo nixos-rebuild switch --flake --sudo --impure && clear; cd -";
-      updatev = "cd /etc/nixos; sudo nix flake update; sudo nixos-rebuild switch --show-trace --flake --sudo --impure && clear; cd -"; # Verbose
-      cnfnix = "cd /etc/nixos && nvim flake.nix && cd -";
-      try = "nix-shell -p";
+    shellAliases = lib.mkMerge [
+      {
+        cnfnix = "cd /etc/nixos && nvim flake.nix && cd -";
+        try = "nix-shell -p";
 
-      todo = "nvim ~/Documents/todo.md";
-      books = "nvim ~/Documents/books.txt";
+        todo = "nvim ~/Documents/todo.md";
+        books = "nvim ~/Documents/books.txt";
 
-      tr = "tree --gitignore -L 3 -a -I .git/ -I __pycache__/ -I target/";
+        tr = "tree --gitignore -L 3 -a -I .git/ -I __pycache__/ -I target/";
 
-      ga = "git add . && clear";
-      gs = "git status";
-      gc = "oco --context ";
-      gcm = "git commit -m ";
-      gp = "git push && clear";
+        ga = "git add . && clear";
+        gs = "git status";
 
-      dumb = "aichat -e";
+        gcm = "git commit -m ";
+        gp = "git push && clear";
 
-      cat = "bat";
-    };
+        cat = "bat";
+      }
+
+      (lib.mkIf (config.networking.hostName == "nixos") {
+        update = /* sh */ ''
+          cd /etc/nixos
+          sudo nix flake update
+          sudo nixos-rebuild switch --flake /etc/nixos#desktop --sudo --impure && clear
+          cd -
+        '';
+
+        # Verbose
+        updatev = /* sh */ ''
+          cd /etc/nixos
+          sudo nix flake update
+          sudo nixos-rebuild switch --show-trace --flake /etc/nixos#desktop --sudo --impure && clear
+          cd -
+        '';
+
+        dumb = "aichat -e";
+        gc = "oco --context ";
+      })
+
+      (lib.mkIf (config.networking.hostName == "nixos-lt") {
+        update = /* sh */ ''
+          cd /etc/nixos
+          sudo nix flake update
+          sudo nixos-rebuild switch --flake /etc/nixos#laptop --sudo --impure && clear
+          cd -
+        '';
+
+        # Verbose
+        updatev = /* sh */ ''
+          cd /etc/nixos
+          sudo nix flake update
+          sudo nixos-rebuild switch --show-trace --flake /etc/nixos#laptop --sudo --impure && clear
+          cd -
+        '';
+
+        gc = "echo 'Opencommit not set up.'";
+      })
+    ];
   };
   programs.bash = {
     enable = true;
